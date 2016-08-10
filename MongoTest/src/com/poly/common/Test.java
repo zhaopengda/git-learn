d702790c2f3d6836942641ddc17a046c0cf9f580
zhaopengda <zhaopengda@zhongchebaolian.com>
2016-08-10 14:28:42 +0800

    改变01

--- MongoTest/src/com/poly/common/Test.java
+++ MongoTest/src/com/poly/common/Test.java
@@ -3,8 +3,11 @@ package com.poly.common;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
- 
+		
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		
 	}
 
 }
